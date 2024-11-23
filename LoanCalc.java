13196778791a69cc5592f05f0abd838558c794ae
amitfidel <amitfidel22@gmail.com>
2024-11-23 16:40:44 +0200

    test 9

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,7 +1,6 @@
 // Computes the periodical payment necessary to pay a given loan.
 public class LoanCalc {
 	
-	
 	static double epsilon = 0.001;  // Approximation accuracy
 	static int iterationCounter;    // Number of iterations 
 	
